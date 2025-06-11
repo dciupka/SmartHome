022fba61717e3d71168461a350d22e20666bbf84
Dawid <dawidciupka@gmail.com>
2025-06-11 13:39:00 +0200

    working app

--- homeautomation/settings.py
+++ homeautomation/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*r#c4_p^wif0nhu^-5-4x3c_67qku(lo=dpef&edi-rgg*cc-o'
+
+# Path to the file containing the secret key
+SECRET_FILE = os.path.join(BASE_DIR, 'secret_key.txt')
+
+
+with open(SECRET_FILE) as f:
+    SECRET_KEY = f.read().strip()
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
